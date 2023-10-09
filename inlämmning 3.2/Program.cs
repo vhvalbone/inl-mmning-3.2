723b52302d305e1514df88818ff2613374c089cb
alsv06003 <alsv06003@E-PW0234JW>
2023-10-09 13:57:37 +0200

    Add project files.

---
+++ inlämmning 3.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace inlämning_3._2;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Har du gått ut gymnasiet än? (j/n): ");
+        char harGåtUtGymnasiet = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (harGåtUtGymnasiet == 'j' || harGåtUtGymnasiet == 'J')
+        {
+            Console.Write("Hur gammal är du? Ange ålder: ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 22)
+            {
+                Console.WriteLine("Vi vill att du jobbor hos oss.");
+            }
+            else
+            {
+                Console.WriteLine("Vi kan tyvärr inte anställa dig då du är för gammal.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Vi kan tyvärr inte anställa dig.");
+        }
+    }
+}
+
